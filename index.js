7c88b74e5a7ce7661900aabd064947d7be7511af
Davin <davin-dj@hotmail.com>
2022-06-27 15:48:57 -0400

    Bonus: Rewrote the code as while loops.

--- index.js
+++ index.js
@@ -4,17 +4,53 @@ let annualExpenses = 0
 
 // Your Code Here
 
-for (let i = weeklyExpenseQuestions.length - 1; i >= 0; i--) {
+// for (let i = weeklyExpenseQuestions.length - 1; i >= 0; i--) {
+//     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+//     weeklyExpenses += answer
+// }
+
+// for (let i = monthlyExpenseQuestions.length - 1; i >= 0; i--) {
+//     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+//     monthlyExpenses += answer
+// }
+
+// for (let i = annualExpenseQuestions.length - 1; i >= 0; i--) {
+//     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+//     annualExpenses += answer
+// }
+
+let i = 0
+//let n = 1
+
+while (weeklyExpenseQuestions[i]) {
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses += answer
+    i++
 }
 
-for (let i = monthlyExpenseQuestions.length - 1; i >= 0; i--) {
+i = 0
+
+while (monthlyExpenseQuestions[i]) {
     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
     monthlyExpenses += answer
+    i++
 }
 
-for (let i = annualExpenseQuestions.length - 1; i >= 0; i--) {
+i = 0
+
+while (annualExpenseQuestions[i]) {
     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
     annualExpenses += answer
+    i++
 }
+
+// while ((i = weeklyExpenseQuestions.length - n) >= 0) {
+//     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+//     weeklyExpenses += answer
+//     if (n < weeklyExpenseQuestions.length) {
+//         n++
+//     }
+//     else {
+//         n = 1
+//     }
+// }
